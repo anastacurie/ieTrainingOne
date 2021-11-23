fa7a8e539e3c9ef0765f920d7b7004de3cff2ef4
anastacurie <anastacurie@gmail.com>
2021-11-23 12:10:39 +0200

    second commit

---
+++ lib/splash.dart
@@ -0,0 +1,15 @@
+
+class SplashRoute extends StatefulWidget {
+  const SplashRoute({ Key? key }) : super(key: key);
+
+  @override
+  _SplashRouteState createState() => _SplashRouteState();
+}
+class _SplashRouteState extends State<SplashRoute> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
